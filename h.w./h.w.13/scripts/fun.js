74040c6d4d5ca7f72451f03d7153dab5ba3c4752
k0teyk0 <k.hulii@hotmail.com>
2020-03-19 16:54:35 +0200

    hw13 done!!!

---
+++ h.w./h.w.13/scripts/fun.js
@@ -0,0 +1,32 @@
+const inputName = document.querySelector(".form-input__name");
+const inputComment = document.querySelector(".form-input__comment");
+const addBtn = document.querySelector(".btn-view");
+const answerForm = document.querySelector(".answer");
+
+addBtn.addEventListener("click", checkEnter);
+
+function checkEnter() {
+  const valueName = inputName.value;
+  const valueComment = inputComment.value;
+
+  if (!valueName) {
+    inputName.classList.add("error");
+    return;
+  } else {
+    inputName.classList.remove("error");
+    if (!valueComment) {
+      inputComment.classList.add("error");
+      return;
+    }
+    inputComment.classList.remove("error");
+  }
+
+  answerForm.innerHTML =
+    answerForm.innerHTML + addComment(valueName, valueComment);
+}
+
+function addComment(value1, value2) {
+  return `<div class="answer__form answer__view"><span class="answer__view_text">${value2}</span>
+<span class="answer__view_name">${value1}</span>
+</div>`;
+}
